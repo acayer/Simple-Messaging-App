877be55b5c3219126702289dc94c5c7e88f79c91
Alex Cayer <alexandrehcayer@gmail.com>
2023-10-20 12:46:26 -0700

    Initial commit

---
+++ Models/MessageModel1.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace WebApplication2.Models
+{
+    public class MessageModel1
+    {
+        private string sender;
+        private string recipient;
+        private string contents;
+        private string timestamp;
+
+        public MessageModel1(string sender, string recipient, string contents, string timestamp)
+        {
+            this.sender = sender;
+            this.recipient = recipient;
+            this.contents = contents;
+            this.timestamp = timestamp;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Messaging App
+ Simple messaging app implemented in C# ASP .Net framework with MVC architecture, running on a Docker container, and stores and retrieves data from a PostgreSQL database. 
